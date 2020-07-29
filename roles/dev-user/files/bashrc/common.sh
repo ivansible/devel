297308415c30aeaa98d8707bff5a57c38ec53d56
Ivan Andreev <ivandeex@gmail.com>
2020-07-29 23:59:07 +0300

    dev-user: new bash aliases, functions, completions
    - factor docker aliases into respective role
    - disambiguate tmux, tail, terraform aliases
    - use less -R on color terminals
    - compatibility alias for resolvectl
    - aliases for systemctl enable/disable
    - improve systemd aliases
    - tmux aliases and completion
    - install oath tool, parallel ssh
    - pssh wrapper for parallel-ssh
    - aliases for traceroute, kubectl, system upgrade
    - more aliases

--- roles/dev-user/files/bashrc/common.sh
+++ roles/dev-user/files/bashrc/common.sh
@@ -1,16 +1,43 @@
 ## ensure history without duplicates
 HISTCONTROL=ignoreboth
 
-[ "$TERM" = screen -a -n "$PROMPT_COMMAND" ] && export PROMPT_COMMAND=""
+if [ "$TERM" = screen ] && [ -n "$PROMPT_COMMAND" ]; then
+    export PROMPT_COMMAND=""
+fi
 
-ph()
-{
-    pygmentize $1 | less
+## common bash functions
+
+ph() {
+    pygmentize "$@" | less -XCSR
 }
 
-totp()
-{
+pssh() {
+    local hosts="${1//,/ }"; shift
+    parallel-ssh -H "$hosts" -i "$@"
+}
+
+## simple replacement for google authenticator
+totp() {
     for _tok in ${TOTP_KEYS//,/ }; do
-        echo "$(oathtool --totp -b ${_tok/*:/}) ${_tok/:*}"
+        echo "$(oathtool --totp -b "${_tok/*:/}") ${_tok/:*}"
     done
 }
+
+sys-upgrade() {
+    local cmd
+    for cmd in update upgrade dist-upgrade autoremove clean; do
+        sudo env DEBIAN_FRONTEND=noninteractive apt-get -qy $cmd
+    done
+}
+
+aws-dist() {
+    awless list distributions |
+    awk -F\| '/Deployed/{print $3,$9}' |
+    sed -r 's/\[ID:|,PublicDNS://g' |
+    awk '{print $2 "\t" $1}' |
+    sort
+}
+
+## get compatible with recent systemd
+which resolvectl >/dev/null || \
+alias resolvectl='sudo systemd-resolve'
